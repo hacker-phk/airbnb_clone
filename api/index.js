9fa6f96a8c45153e06cce49ce9f841a27f759146
Hemanthkumar <107429016+hacker-phk@users.noreply.github.com>
2024-07-30 08:35:54 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -29,7 +29,7 @@ app.use("/uploads", express.static(__dirname + "/uploads"));
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:5173",
+    origin: "https://airbnb-clone-frontend-khaki.vercel.app",
   })
 );
 app.use(cookieParser());
